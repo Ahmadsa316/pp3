9bc59bad3f3107c64f289eb12dd208ea3974462f
Ahmad Sarfaraz <ahmadsarfaraz0966@gmail.com>
2025-11-10 14:34:18 +0200

    Step 5: Implement Third-Party Middleware

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require ('express');
-const morgan = require ('morgan');
-const tourRouter = require('/routes/tourRouter');
-const userRouter = require ('/routes/userRouter');
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRouter');
+const userRouter = require('./routes/userRouter');
+
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan('tiny'));
 
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
-app.get ('/', (req, res) => {
-res.status(200).json({
-message: 'Welcome to the Tours & Users API',
-status: 'success'
-});
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to the Tours & Users API 🚀',
+    status: 'success'
+  });
 });
 
 module.exports = app;
